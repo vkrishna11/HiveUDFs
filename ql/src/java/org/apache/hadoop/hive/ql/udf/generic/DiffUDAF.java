067c0c70df81d64b737aaf78db46f3b2cd65a58d
Vamshi Devraj <vkrishna11@gmail.com>
2020-01-24 10:43:13 +0100

    recent changes

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/DiffUDAF.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/DiffUDAF.java
@@ -1,23 +1,5 @@
 package org.apache.hadoop.hive.ql.udf.generic;
 
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -89,11 +71,7 @@ public class DiffUDAF extends DiffUDAFAbstract {
         ArrayList<Object> listOfValues;
         ArrayList<Object> listisSame;
         ArrayList<Boolean> isSame;
-
-
         boolean isFirstValue;
-
-
         int parmIndex;
 
 
@@ -103,7 +81,6 @@ public class DiffUDAF extends DiffUDAFAbstract {
             isSame = new ArrayList<>();
             listOfValues = new ArrayList<>();
             listisSame = new ArrayList<>();
-
             this.isFirstValue = true;
             parmIndex = 0;
         }
@@ -111,24 +88,26 @@ public class DiffUDAF extends DiffUDAFAbstract {
 
         public void addRow(Object currValue, int parm_Number, boolean isSameAsPrevious, ObjectInspector inputObject) {
 
-
+            // isSameAsPrevous - identifies if the current value is similar to previous value
             if (isSameAsPrevious) {
-
                 if (parm_Number == 0) {
                     values.remove(parm_Number);
                     isSame.remove(parm_Number);
-                    // listOfValues.set(parm_Number, values);
-                    //listisSame.set(parm_Number, isSame);
                 } else {
 
                     ArrayList<Object> tempVal = (ArrayList<Object>) listOfValues.get(parm_Number);
-                    tempVal.set(0, null);
+                   if(!tempVal.isEmpty()) {
+                       // tempVal.set(0, null);
+                       tempVal.remove(0);
+                   }
                     ArrayList<Object> tempSame = (ArrayList<Object>) listisSame.get(parm_Number);
-                    tempSame.set(0, false);
+                   if(!tempSame.isEmpty()) {
+                       // tempSame.set(0, false);
+                       tempSame.remove(0);
+                   }
                 }
-
-
             }
+
             String tempString;
             Text currText;
             ArrayList<Object> resList;
@@ -250,8 +229,7 @@ public class DiffUDAF extends DiffUDAFAbstract {
                                 res = ((ArrayList<Object>) lb.listOfValues.get(i)).get(0);
                             } else {
 
-                                // resList.add(((ArrayList<Object>) lb.listOfValues.get(i)).get(0));
-                                // ObjOI = super.getInputOI();
+
                                 if (ObjOI[0].getCategory() == ObjectInspector.Category.LIST) {
                                     ArrayList<Object> resList = new ArrayList<>();
                                     resList.add(0, ((ArrayList<Object>) lb.listOfValues.get(i)).get(0));
--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/DiffUDAFAbstract.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/DiffUDAFAbstract.java
@@ -1,23 +1,5 @@
 package org.apache.hadoop.hive.ql.udf.generic;
 
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -56,6 +38,9 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
 
 
         // Validate input parameter data types...
+        // First argument to the function could be a collection data type, however,
+        // subsequent data types must be primitive only...
+
         for (int i = 0; i < paramOIs.length; i++) {
             switch (paramOIs[i].getCategory()) {
                 case PRIMITIVE:
@@ -89,12 +74,9 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
             }
         }
 
-        // ObjectInspector amtOI = paramOIs[0];
-
-
         GenericUDAFDiffAbstractEvaluator eval = createDiffEvaluator();
-        // eval.setOffset_amt(1);
-        return eval;  // Return GenericUDAFEvaluator
+
+        return eval;  // Return GenericUDAFEvaluator instance
     }
 
     protected abstract String functionName();
@@ -104,17 +86,15 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
     public static abstract class GenericUDAFDiffAbstractEvaluator extends GenericUDAFEvaluator {
 
         private transient ObjectInspector[] inputOI;
-        // private int offset_amt;
-        //private transient Object[] previousObj = null; // to hold previous value, for comparision
-        private ArrayList<Object> previousObj = null;
+        private ArrayList<Object> previousObj = null; // ArrayList to hold previous values...
         private String fnName;
         private transient ListObjectInspector listOI;
         private transient PrimitiveObjectInspector primitiveOI;
         private transient StructObjectInspector structOI;
         private transient MapObjectInspector mapOI;
         private int noOfParams;
-        private ArrayList<ObjectInspector> objInspectors;
-        //boolean restorepls = false;
+        private ArrayList<ObjectInspector> objInspectors;  // ArrayList to hold Object Inspectors
+
 
         public GenericUDAFDiffAbstractEvaluator() {
         }
@@ -124,7 +104,6 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
          */
         protected GenericUDAFDiffAbstractEvaluator(GenericUDAFDiffAbstractEvaluator src) {
             this.inputOI = src.inputOI;
-            // this.offset_amt = 1; // set default
             this.fnName = src.fnName;
             this.mode = src.mode;
             this.previousObj = src.previousObj;
@@ -132,7 +111,7 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
             this.listOI = src.listOI;
             this.mapOI = src.mapOI;
             this.structOI = src.structOI;
-            this.noOfParams=src.noOfParams;
+            this.noOfParams = src.noOfParams;
             this.objInspectors = src.objInspectors;
         }
 
@@ -142,57 +121,62 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
 
         @Override
         public ObjectInspector init(Mode m, ObjectInspector[] parameters) throws HiveException {
+
             super.init(m, parameters);
-            this.objInspectors = new ArrayList<>();
+            this.objInspectors = new ArrayList<>(); // Initialize ObjectInspector ArrayList
+
             // Diff only requires "MAP" phase, hence accepts only "COMPLETE" mode...
             if (m != Mode.COMPLETE) {
                 throw new HiveException("Only COMPLETE mode supported for " + fnName + " function");
             }
+
             this.noOfParams = parameters.length;
             inputOI = parameters;
+
+            // Loop through all the arguments & populate ObjectInspector ArrayList...
             for (int i = 0; i < parameters.length; i++) {
                 switch (inputOI[i].getCategory()) {
                     case PRIMITIVE:
                         primitiveOI = (PrimitiveObjectInspector) inputOI[i];
-                        objInspectors.add(i,primitiveOI);
+                        objInspectors.add(i, primitiveOI);
                         break;
                     case LIST:
                         listOI = (ListObjectInspector) inputOI[i];
-                        objInspectors.add(i,listOI);
+                        objInspectors.add(i, listOI);
                         break;
                     case STRUCT:
                         structOI = (StructObjectInspector) inputOI[i];
-                        objInspectors.add(i,structOI);
+                        objInspectors.add(i, structOI);
                         break;
                     case MAP:
                         mapOI = (MapObjectInspector) inputOI[i];
-                        objInspectors.add(i,mapOI);
+                        objInspectors.add(i, mapOI);
                         break;
                 }
             }
-            if(inputOI[0].getCategory() != ObjectInspector.Category.PRIMITIVE){
+
+            // Output data type must be  "ArrayList<String>" for all PRIMITIVE input argument (first argument)...
+            // regardless of Double, Integer etc.
+            // ArrayList is mandatory as PivotResult=TRUE
+
+            if (inputOI[0].getCategory() != ObjectInspector.Category.PRIMITIVE) {
                 return ObjectInspectorFactory.getStandardListObjectInspector(ObjectInspectorUtils.
                         getStandardObjectInspector(ObjectInspectorFactory.getStandardListObjectInspector(PrimitiveObjectInspectorFactory.writableStringObjectInspector)));
-            }
-            else {
+            } else {
+                // and "ArrayList<ArrayList<String>>" for LIST input parameter (first argument)...
+                // regardless of ArrayList<Double>, ArrayList<Integer> etc...
+                // Outer ArrayList is mandatory as PivotResult=TRUE
+
                 return ObjectInspectorFactory.getStandardListObjectInspector(ObjectInspectorUtils.
                         getStandardObjectInspector(PrimitiveObjectInspectorFactory.writableStringObjectInspector));
             }
 
-            //  return PrimitiveObjectInspectorFactory.writableStringObjectInspector;
 
             // Set the output result data type same as the first input parameter
            /* return ObjectInspectorFactory.getStandardListObjectInspector(ObjectInspectorUtils
                     .getStandardObjectInspector(parameters[0]));*/
         }
 
-        /*public int getOffset_amt() {
-            return this.offset_amt;
-        }
-
-        public void setOffset_amt(int offset_amt) {
-            this.offset_amt = offset_amt;
-        }*/
 
         public String getFnName() {
             return fnName;
@@ -211,16 +195,16 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
         @Override
         public AggregationBuffer getNewAggregationBuffer() throws HiveException {
             IDiffBuffer lb = getNewDiffBuffer();
-            //  lb.initialize(offset_amt);
             lb.initialize();
+            // initialize previousObj ArrayList
             this.previousObj = new ArrayList<>(this.noOfParams);
-           // this.restorepls = false;
+
             return lb;
         }
 
         @Override
         public void reset(AggregationBuffer agg) throws HiveException {
-            //  ((LeadLagBuffer) agg).initialize(offset_amt);
+
             ((IDiffBuffer) agg).initialize();
         }
 
@@ -228,32 +212,31 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
         public void iterate(AggregationBuffer agg, Object[] parameters) throws HiveException {
             Object rowExprVal;
 
-            boolean isSame;
+            boolean isSame; // identifies if the current row is same as previous row
             boolean previousExists;
-            // int i = 0;
+
             for (int i = 0; i < parameters.length; i++) {
 
                 rowExprVal = ObjectInspectorUtils.copyToStandardObject(parameters[i], inputOI[i]);
 
                 isSame = true;
 
-                //boolean previousExists;
+                // Read one by one arguments in a loop, using respective Object Inspectors
                 switch (inputOI[i].getCategory()) {
                     case PRIMITIVE:
-
-                        //   PrimitiveObjectInspector prim1 = (PrimitiveObjectInspector) inputOI[0];
+                        // For String...
                         if (((PrimitiveObjectInspector) objInspectors.get(i)).getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.STRING) {
                             // retrieve current row as a String
                             String currentStr = (String) ((PrimitiveObjectInspector) objInspectors.get(i)).getPrimitiveJavaObject(rowExprVal);
-                            // If previous row exists, then retrieve it as a String
+                            // If previous row exists, then compare with current row
                             if (!previousObj.isEmpty() && previousObj.size() > i) {
 
                                 String previousStr = (String) ((PrimitiveObjectInspector) objInspectors.get(i)).getPrimitiveJavaObject(previousObj.get(i));
-                                // compare current & previous row...
                                 if (!currentStr.equals(previousStr)) {
                                     isSame = false;
                                 }
                             }
+                            // For Integer...
                         } else if (((PrimitiveObjectInspector) objInspectors.get(i)).getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.INT) {
                             // retrieve current row as a Integer
                             Integer currentInt = (Integer) ((PrimitiveObjectInspector) objInspectors.get(i)).getPrimitiveJavaObject(rowExprVal);
@@ -266,6 +249,7 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
                                     isSame = false;
                                 }
                             }
+                            // For Double
                         } else if (((PrimitiveObjectInspector) objInspectors.get(i)).getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.DOUBLE) {
                             // retrieve current row as a Double
                             Double currentDouble = (Double) ((PrimitiveObjectInspector) objInspectors.get(i)).getPrimitiveJavaObject(rowExprVal);
@@ -287,12 +271,13 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
                         break;
                     case LIST:
 
-                        //ListObjectInspector list1 = (ListObjectInspector) inputOI[0];
-                        PrimitiveObjectInspector elementOI = (PrimitiveObjectInspector) listOI.getListElementObjectInspector();
-                        if(elementOI.getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.STRING) {
-                            // retrieve current & previous rows from the list and compare the values...
-                            List<Text> currentList = (List<Text>) listOI.getList(rowExprVal);
 
+                        PrimitiveObjectInspector elementOI = (PrimitiveObjectInspector) listOI.getListElementObjectInspector();
+                        // For String...
+                        if (elementOI.getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.STRING) {
+                            // retrieve current row
+                            List<Text> currentList = (List<Text>) listOI.getList(rowExprVal);
+                            // retrieve previous row and compare with current row
                             if (!previousObj.isEmpty() && previousObj.size() > i) {
 
                                 List<Text> previousList = (List<Text>) listOI.getList(previousObj.get(i));
@@ -306,7 +291,8 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
                                 }
                             }
                         }
-                        else if(elementOI.getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.INT) {
+                        // For Integer...
+                        else if (elementOI.getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.INT) {
                             List<IntWritable> currentList = (List<IntWritable>) listOI.getList(rowExprVal);
 
                             if (!previousObj.isEmpty() && previousObj.size() > i) {
@@ -322,7 +308,8 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
                                 }
                             }
                         }
-                        else if(elementOI.getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.DOUBLE) {
+                        // For Double
+                        else if (elementOI.getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.DOUBLE) {
                             List<DoubleWritable> currentList = (List<DoubleWritable>) listOI.getList(rowExprVal);
 
                             if (!previousObj.isEmpty() && previousObj.size() > i) {
@@ -350,34 +337,15 @@ public abstract class DiffUDAFAbstract extends AbstractGenericUDAFResolver {
                 }
 
 
-                /*if (restorepls) {
-                    rowExprVal = previousObj.get(i);
-                    restorepls = false;
-                } else {
-                   // previousObj.set(i,rowExprVal);
-                   if(!previousObj.isEmpty()) {
-                       previousObj.remove(i);
-                   }
-                    previousObj.add(i, rowExprVal);
-                }*/
+                // store current value into previous value
                 if (!previousObj.isEmpty() && previousObj.size() > i) {
-                    //previousObj.remove(i);
-                    previousObj.set(i,rowExprVal);
+
+                    previousObj.set(i, rowExprVal);
                 } else {
                     previousObj.add(i, rowExprVal);
                 }
 
-
-                // if the previous row exists and is the same as the previous row,
-                // then set the result value as "null".
-                /*if (isSame && previousExists) {
-                    rowExprVal = null;
-                    restorepls = true;
-                }*/
-
-
-                //  ((IDiffBuffer) agg).addRow(rowExprVal, defaultVal);
-                ((IDiffBuffer) agg).addRow(rowExprVal, i, !previousExists ? false : isSame,inputOI[i]);
+                ((IDiffBuffer) agg).addRow(rowExprVal, i, !(previousExists) ? false : isSame, inputOI[i]);
 
             }
         }
